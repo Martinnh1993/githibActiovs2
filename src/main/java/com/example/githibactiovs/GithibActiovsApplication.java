fa1e5bd18414f7fb685f9ec8d2149b5dfc0963cf
Marti <Martinnh1993@gmail.com>
2022-08-29 14:42:06 +0200

    Initial commit

---
+++ src/main/java/com/example/githibactiovs/GithibActiovsApplication.java
@@ -0,0 +1,13 @@
+package com.example.githibactiovs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithibActiovsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithibActiovsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githibactiovs/GithibActiovsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githibactiovs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithibActiovsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
